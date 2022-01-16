2ab20bcfb1b75a871a45435aed64112b637230e9
Paul Lockyer <plockyer@googlemail.com>
2022-01-16 21:27:04 +0000

    Removed unwanted code and added express error handling and deafult route

--- app.js
+++ app.js
@@ -1,41 +1,55 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require('express')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Import the routes for the app
+const dbTest = require('./routes/db_test.js')
 
-var app = express();
+// Create the express app server
+var app = express()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// JSON config
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ type: 'application/json'}))
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Assign any routes
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use('/db_test', dbTest)
+
+// handle unknown routes
+app.get('*',(req,res,next) => {
+    const err = new Error("endpoint not found");
+    err.status = 404;
+    next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// Error Handling
+app.use((error, req, res, next) => {
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    let statusCode;
+    let message;
+
+    // Check for DB related error messages
+    if(error.code == 23505){
+        statusCode = 400;
+        message = "The requested data could not be added due to violating uniquness rules";
+    } else {
+        statusCode = error.status || 500;
+        message = error.message || "Internal server error";
+    }
+
+    res.status(statusCode).json({
+        error: {
+            status: statusCode,
+            dbErrorCode: error.code,
+            message: message,
+        }
+    });
 });
 
-module.exports = app;
+app.listen(3000, () => {
+    console.log('App listening on port 3000')
+})
