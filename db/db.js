301e47e6b10804c176b86ba31d19f3f42c6ac19c
Paul Lockyer <plockyer@googlemail.com>
2022-01-18 11:35:24 +0000

    Added function to cleanup a specified table of all data i.e. delet it ALL

--- db/db.js
+++ db/db.js
@@ -18,8 +18,26 @@ module.exports = {
         const start = Date.now();
         return await pool.query(text, params, (err, res) => {
             const duration = Date.now() - start;
-            console.log('Executed query', {text, duration, rows: res.rowCount})
+        
+            // Get the rowcount if applicable
+            let rowCount;
+            if(!res){
+                rowCount = 0;
+            } else {
+                rowCount = res.rowCount;
+            }
+
+            console.log('Executed query', {text, duration, rows: rowCount})
             callback(err, res);
         });
+    },
+    // Clean out a table of all records, useful for clearing out testing data
+    cleanTable: async (table_name, callback) => {
+        // Only run if we have a table_name set
+        if(!table_name){
+            console.log({message: "You must provide a table name to delete from"})
+        } else {
+            await pool.query("DELETE FROM " + table_name);
+        }
     },
 };
