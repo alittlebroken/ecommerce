4f9c0c15b5496ef89528007d3a36f69325cca55e
Paul Lockyer <plockyer@googlemail.com>
2022-02-07 09:18:49 +0000

    Added passport authentication config

---
+++ config/passport.js
@@ -0,0 +1,73 @@
+// Required packages and classes
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const JWTstrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+const userModel = require('../models/user');
+const bcrypt = require('bcrypt')
+require('dotenv').config()
+
+// Create a user
+passport.use(
+    'signup',
+    new localStrategy(
+        {
+            usernameField: 'email',
+            passwordField: 'password'
+        },
+        async (email, password, done) => {
+            try {
+                const user = await userModel.create({ email, password });
+
+                return done(null, user);
+            } catch(err) {
+                done(err);
+            }
+        }
+    )
+);
+
+// Login a user
+passport.use(
+    'login',
+    new localStrategy(
+        {
+            usernameField: 'email',
+            passwordField: 'password'
+        },
+        async (email, password, done) => {
+            try{
+                const user = await userModel.findByEmail(email);
+
+                if(!user){
+                    return done(null, false, { message: 'user not found'});
+                }
+                
+                const validate = await userModel.verifyPassword(user.password,password);
+                if(!validate){
+                    return done(null, false, { message: 'Wrong password'});
+                }
+
+                return done(null, user, { message: 'Logged in successfully'});
+            } catch(error) {
+                return done(error);
+            }
+        }
+    )
+);
+
+// Verify the JWT token
+passport.use(
+    new JWTstrategy({
+        secretOrKey: process.env.TOKEN_SECRET,
+        jwtFromRequest: ExtractJWT.fromUrlQueryParameter('secret_token')
+    },
+    async(token, done) => {
+        try{
+            return done(null, token.user);
+        } catch(error) {
+            done(error);
+        }
+    }
+    )
+);
