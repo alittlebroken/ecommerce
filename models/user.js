7a006ca49b9d0cea148522dcad4351fb9777cc99
Paul Lockyer <plockyer@googlemail.com>
2022-02-07 09:37:31 +0000

    Added constructor to userModel and added methods for hashing passwords and generating access JWT access tokens

--- models/user.js
+++ models/user.js
@@ -1,8 +1,20 @@
 // require needed packages
-const createHttpError = require('http-errors');
 const db = require('../db/db')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 module.exports  = class userModel {
+
+    // Class constructor
+    constructor(data = {}){
+        this.id = data.id || null,
+        this.email = data.email || null,
+        this.password = data.password || null,
+        this.is_admin = data.is_admin || false,
+        this.is_logged_in = data_is_logged_in || false,
+        this.last_logon = data.last_logon || null
+    }
 
     // find all users
     static async find () {
@@ -13,7 +25,7 @@ module.exports  = class userModel {
         
 
             // Check we have some records
-            if(result.rows.length){
+            if(result.rows?.length){
                 return result.rows;
             }
 
@@ -35,7 +47,11 @@ module.exports  = class userModel {
             const result = await db.query(query,values,(err, res) => {});
 
             // Check we have a record or more
-            if(result.rows.length){
+            if(result.rows?.length){
+                // Assign local vars the required data from the returned resultset
+                this.id = result.rows[0].user_id;
+                this.email = result.rows[0].email;
+                this.password = result.rows[0].password;
                 return result.rows[0];
             }
 
@@ -57,7 +73,11 @@ module.exports  = class userModel {
             const result = await db.query(query,values,(err, res) => {});
 
             // Check we have a record or more
-            if(result.rows.length){
+            if(result.rows?.length){
+                // Assign local vars the required data from the returned resultset
+                this.id = result.rows[0].user_id;
+                this.email = result.rows[0].email;
+                this.password = result.rows[0].password;
                 return result.rows[0];
             }
 
@@ -72,16 +92,26 @@ module.exports  = class userModel {
         try{
 
             // Create the query
-            const query = "INSERT INTO users(email,password,forename, surname) VALUES($1, $2, $3, $4) RETURNING user_id;";
+            const query = "INSERT INTO users(email,password,forename, surname) VALUES($1, $2, $3, $4) RETURNING *;";
             
             // Get the required values
             const { email, password, forename, surname } = data;
-                
+            
+            // Generate a hashed password
+            const hash = await this.hashPassword(password);
+            console.log(hash)
+            
             // Run the query
-            const result = await db.query(query,[email, password, forename, surname],(err, res) => {});
+            const result = await db.query(query,[email, hash, forename, surname]);
 
             // Check we have a record or more
             if(result.rows.length){
+                // Assign local vars the required data from the returned resultset
+                this.id = result.rows[0].user_id;
+                this.email = result.rows[0].email;
+                this.password = result.rows[0].password;
+
+                // Send back the result to the calling script
                 return result.rows[0];
             }
 
@@ -135,6 +165,23 @@ module.exports  = class userModel {
         } catch(err) {
             throw new Error(err)
         }
+    }
+
+    // Creates a hashed password for the user
+    static async hashPassword(password) {
+        return await bcrypt.hash(password, process.env.SALT_ROUNDS);
+    }
+
+    // Returns true if passwords match, false otherwise
+    static async verifyPassword(hash, password){
+        return await bcrypt.compare(password, hash);
+    }
+
+    // Generates a jason web token used in authorizing the user
+    static async generateAccessToken(data){
+
+        return jwt.sign(data, process.env.TOKEN_SECRET);
+
     }
 
 }
