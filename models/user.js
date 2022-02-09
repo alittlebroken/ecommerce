0b3a1c92e73fad2feb9237c17751e722222e6839
Paul Lockyer <plockyer@googlemail.com>
2022-02-09 17:28:18 +0000

    Changed to bcryptjs from bcrypt and fixed typo for adding roles to userModel constructor

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 // require needed packages
 const db = require('../db/db')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
@@ -14,7 +14,7 @@ module.exports  = class userModel {
         this.is_admin = data.is_admin || false,
         this.is_logged_in = data_is_logged_in || false,
         this.last_logon = data.last_logon || null
-        this.role = data.roles || 'Customer'
+        this.roles = data.roles || 'Customer'
     }
 
     // find all users
@@ -39,20 +39,22 @@ module.exports  = class userModel {
 
     static async findByEmail (email){
         try{
-
+            console.log(email)
             // Create the query
             const query = "SELECT * FROM users WHERE email = $1;";
             const values = [email];
 
             // Run the query
-            const result = await db.query(query,values,(err, res) => {});
-
+            const result = await db.query(query,values);
+            console.log(result)
             // Check we have a record or more
             if(result.rows?.length){
+                
                 // Assign local vars the required data from the returned resultset
                 this.id = result.rows[0].user_id;
                 this.email = result.rows[0].email;
                 this.password = result.rows[0].password;
+                this.roles = result.rows[0].roles;
                 return result.rows[0];
             }
 
@@ -91,26 +93,46 @@ module.exports  = class userModel {
 
     static async create (data){
         try{
-
+            
             // Create the query
-            const query = "INSERT INTO users(email,password,forename,surname) VALUES($1, $2, $3, $4) RETURNING *;";
-            
+            const queryString = `INSERT INTO users(email,password,forename,\
+                surname,contact_number) VALUES($1, $2, $3, $4, $5) \
+                RETURNING *;`;
+
             // Get the required values
-            const { email, password, forename, surname } = data;
-            
+            const { 
+                email, 
+                password,
+                forename,
+                surname,
+                contact_number
+            } = data;
+        
             // Generate a hashed password
-            const salt = await bcrypt.genSalt(31);
-            const hash = await this.hashPassword(password, salt);
-            
+            const hash = await bcrypt.hash(password, 10);
+
+            // Set the values to use
+            const values = [
+                email,
+                hash,
+                forename,
+                surname,
+                contact_number
+            ];
+
             // Run the query
-            const result = await db.query(query,[email, hash, forename, surname]);
+            const result = await db.pool.query(queryString,values);
 
             // Check we have a record or more
-            if(result.rows.length){
+            if(result?.rows?.length){
                 // Assign local vars the required data from the returned resultset
                 this.id = result.rows[0].user_id;
                 this.email = result.rows[0].email;
                 this.password = result.rows[0].password;
+                this.forename = result.rows[0].forename;
+                this.surname = result.rows[0].surname;
+                this.contact_number = result.rows[0].contact_number;
+                this.roles = result.rows[0].roles;
 
                 // Send back the result to the calling script
                 return result.rows[0];
@@ -119,8 +141,7 @@ module.exports  = class userModel {
             return null;
 
         } catch(err) {
-            
-            throw new Error(err)
+            throw new Error(err);
         }
     }
 
@@ -171,7 +192,17 @@ module.exports  = class userModel {
 
     // Creates a hashed password for the user
     static async hashPassword(password) {
-        return await bcrypt.hash(password, parseInt(process.env.SALT_ROUNDS));
+        
+        // Attempt to hash the password
+        try{
+
+            const hashedPassword = await bcrypt.hash(password, 10);
+            return hashedPassword;
+
+        } catch(err) {
+            console.log(err)
+        }
+        
     }
 
     // Returns true if passwords match, false otherwise
@@ -182,7 +213,11 @@ module.exports  = class userModel {
     // Generates a jason web token used in authorizing the user
     static async generateAccessToken(data){
 
-        return jwt.sign(data, process.env.TOKEN_SECRET, { expiresIn: process.env.TOKEN_EXPIRATION});
+        return jwt.sign(
+            data, 
+            process.env.TOKEN_SECRET, 
+            { expiresIn: process.env.TOKEN_EXPIRATION}
+        );
 
     }
 
