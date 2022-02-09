30d904399b3e5ce39e8c7cee2543f60b58becf3f
Paul Lockyer <plockyer@googlemail.com>
2022-02-09 17:23:13 +0000

    Updated any next methods to use correct error vars and use spread notation for roles in function parameter

--- utils/auth.js
+++ utils/auth.js
@@ -5,20 +5,22 @@
 */
 
 // Check the roles of the user
-const checkUserRoles = (roles) => (req, res, next) => {
+const checkUserRoles = (...roles) => (req, res, next) => {
     // Do we have a valid user in the request
+
     if(!req.user){
         const error = new Error('You must be logged in to access this resource.');
         error.status = 401;
-        next(err);
+        next(error);
     }
 
     // Check the user is in one of the roles passed in
-    const inRole = roles.find(role => req.user.role === role);
+    const inRole = roles.find(role => req.user.roles === role);
+    
     if(!inRole){
-        const error = new Error('You do not have access this resource.');
+        const error = new Error('You do not have access to this resource.');
         error.status = 401;
-        next(err);
+        next(error);
     }
 
     // User is in role so just go onto the next middleware
