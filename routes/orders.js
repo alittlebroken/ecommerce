1a7c4e99da3295ad79567b8ace6a59d355d9c3c1
Paul Lockyer <plockyer@googlemail.com>
2022-02-11 14:57:36 +0000

    Added authentication using passport to the routes

--- routes/orders.js
+++ routes/orders.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router()
 const db = require('../db/db')
+const passport = require('passport')
+
+// Load any utils
+const ROLES = require('../utils/roles');
+const UTILS = require('../utils/auth');
 
 // require some of the order models
 const orderModel = require('../models/order')
@@ -35,14 +40,19 @@ router.param('orderid', (req, res, next, orderid) => {
 
 // Create the routes
 
-// Route for getting all orders
-router.get('/', async (req, res, next) => {
+// Route for getting all orders, ADMINS only
+router.get(
+    '/', 
+    passport.authenticate('jwt', { session: false }), 
+    UTILS.checkUserRoles(ROLES.Admin),
+    async (req, res, next) => {
 
     // Query the DB and get a list of all orders
     try{
 
         // Get a list of orders
-        const result = await orderModel.findOrders();
+        const orders = new orderModel();
+        const result = await orders.findOrders();
 
         // Check we have some orders to get
         if(result?.length){
@@ -60,8 +70,12 @@ router.get('/', async (req, res, next) => {
 
 });
 
-// Route for getting an individual Order from the DB
-router.get('/:orderid', async (req, res, next) => {
+// Route for getting an individual Order from the DB, ADMINS only
+router.get(
+    '/:orderid',
+    passport.authenticate('jwt', { session: false }), 
+    UTILS.checkUserRoles(ROLES.Admin), 
+    async (req, res, next) => {
 
     // Extract the ID from the request body
     const id = req.body.orderId;
@@ -69,7 +83,8 @@ router.get('/:orderid', async (req, res, next) => {
     // Query the DB and return the result
     try{
 
-        const result = await orderModel.findOrder(id);
+        const orders = new orderModel({ order_id: id });
+        const result = await orders.findOrder();
         
         if(result?.length){
             res.status(200).json(result);
@@ -86,20 +101,32 @@ router.get('/:orderid', async (req, res, next) => {
 
 });
 
-// Update an order
-router.put('/:orderid', async (req, res, next) => {
+// Update an order, ADMINS
+router.put(
+    '/:orderid',
+    passport.authenticate('jwt', { session: false }), 
+    UTILS.checkUserRoles(ROLES.Admin), 
+    async (req, res, next) => {
 
     // Get the ID from the req body
     const id = req.body.orderId;
 
     // Get the updates from the req body
     const updates = req.body.updates;
-    if(!updates?.length) return res.status(404);
+
+
+    if(!updates?.length || updates == undefined || updates == null) {
+        res.status(404).json({
+            status: 404,
+            message: 'No updates found'
+        });
+    }
 
     // Attempt to update
     try{
         
-        const result = await orderModel.update({updates, order_id: id});
+        const order = new orderModel();
+        const result = await order.update({updates, order_id: id});
         
         if(result.length){
             res.status(201).json(result);
@@ -117,8 +144,12 @@ router.put('/:orderid', async (req, res, next) => {
 
 });
 
-// Delete an order
-router.delete('/:orderid', async (req, res, next) => {
+// Delete an order, ADMINS only
+router.delete(
+    '/:orderid',
+    passport.authenticate('jwt', { session: false }), 
+    UTILS.checkUserRoles(ROLES.Admin), 
+    async (req, res, next) => {
 
     // Get the ID from the request body
     const id = req.body.orderId;
