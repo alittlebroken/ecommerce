a6d2f6568148687a17cc96ad13bcd8957ccd0a73
Paul Lockyer <plockyer@googlemail.com>
2022-05-05 14:55:46 +0100

    added debug output in getUsersHashedPassword method

--- models/user.js
+++ models/user.js
@@ -197,6 +197,7 @@ module.exports  = class userModel {
              * Execute the query and check if we have any data returned
              */
             const result = db.query(stmt, values);
+            console.log(result)
             if(result.rows?.length){
 
                 /**
