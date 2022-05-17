9d6ff0f7daffd886b9e928fde5afe3e6109033ae
Paul Lockyer <plockyer@googlemail.com>
2022-05-17 15:43:41 +0100

    Update setup_db.js

--- setup_db.js
+++ setup_db.js
@@ -8,9 +8,9 @@ const pgpool = new Pool({
     database: process.env.POSTGRES_DB
 });
 
-const table = 'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))'
-const text = 'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *'
-const values = ['Mona the', 'Octocat', 9, '88 Colin P Kelly Jr St, San Francisco, CA 94107, United States', 'octocat@github.com']
+const table = 'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))';
+const text = 'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *';
+const values = ['Mona the', 'Octocat', 9, '88 Colin P Kelly Jr St, San Francisco, CA 94107, United States', 'octocat@github.com'];
 
 (async () => {
    
@@ -21,7 +21,7 @@ const values = ['Mona the', 'Octocat', 9, '88 Colin P Kelly Jr St, San Francisco
         throw new Error(error);
     }
     
-})()
+})();
 
 /**
 pgpool.query(text, values, (err, res) => {
