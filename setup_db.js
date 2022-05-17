8ffb14cde4fd2a43b219a5ddbe78eb813588bac0
Paul Lockyer <plockyer@googlemail.com>
2022-05-17 14:02:17 +0100

    Update setup_db.js

--- setup_db.js
+++ setup_db.js
@@ -3,12 +3,13 @@ const { Client } = require('pg');
 const pgclient = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
-    user: 'testdbusr',
-    password: 'testpassword',
-    database: 'test'
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB
 });
 
 pgclient.connect();
+console.log(pgclient)
 
 const table = 'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))'
 const text = 'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *'
