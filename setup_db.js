96a4f47e99ac69f63dafa9932c97c400919e9d30
Paul Lockyer <plockyer@googlemail.com>
2022-05-17 17:10:29 +0100

    added test data to test table

--- setup_db.js
+++ setup_db.js
@@ -8,9 +8,6 @@ const pgpool = new Pool({
     database: process.env.POSTGRES_DB
 });
 
-console.log(`\nprocess.env.POSTGRES_PASSWORD\n`)
-
-
 const table = 'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))';
 const text = 'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *';
 const values = ['Mona the', 'Octocat', 9, '88 Colin P Kelly Jr St, San Francisco, CA 94107, United States', 'octocat@github.com'];
@@ -18,8 +15,21 @@ const values = ['Mona the', 'Octocat', 9, '88 Colin P Kelly Jr St, San Francisco
 (async () => {
    
     try{
+        /**
+         * Create the database table
+         */
         const tableResult = await pgpool.query(table);
-        console.log(tableResult);
+        
+        /**
+         * Insert the test data
+         */
+        if(tableResult){
+            
+            const insertResult = await pgpool.query(text, values);
+            console.log(insertResult?.rows);
+            
+        }
+        
     } catch(error) {
         console.log('Error Encountered');
         console.log(error);
@@ -29,10 +39,6 @@ const values = ['Mona the', 'Octocat', 9, '88 Colin P Kelly Jr St, San Francisco
 })();
 
 /**
-pgpool.query(text, values, (err, res) => {
-    if (err) throw err
-});
-
 pgpool.query('SELECT * FROM student', (err, res) => {
     if (err) throw err
     console.log(err, res.rows) // Print the data in student table
