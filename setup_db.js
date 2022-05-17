ed8c427ac3e5239aa4da24cef1cef5fd38b8b1bc
Paul Lockyer <plockyer@googlemail.com>
2022-05-17 17:02:37 +0100

    adding debug info

--- setup_db.js
+++ setup_db.js
@@ -8,6 +8,9 @@ const pgpool = new Pool({
     database: process.env.POSTGRES_DB
 });
 
+console.log(`\nprocess.env.POSTGRES_PASSWORD\n`)
+
+
 const table = 'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))';
 const text = 'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *';
 const values = ['Mona the', 'Octocat', 9, '88 Colin P Kelly Jr St, San Francisco, CA 94107, United States', 'octocat@github.com'];
